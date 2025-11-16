f197948b0a97fdd4d0396ba5f16332339f355838
SterileMilk <steralizedmilk@gmail.com>
2025-11-16 10:00:32 +0000

    Add files via upload

---
+++ 0.1 Drawing/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  //eyes
+  fill(64, 224, 208)
+  circle (50, 50, 50);
+  circle (200, 50, 50);
+  
+  //mouth
+  fill(34, 139, 34)
+  ellipse(150, 150, 200, 20);  
+}
